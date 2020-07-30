198e3ccbd6ccb58f7a6adfb40003da673aa371e0
Matt <matthewlukebyrne@gmail.com>
2020-07-30 19:27:05 +0100

    build folder add! 1.0.1

---
+++ build/precache-manifest.90268079bedc55d62b0fb354f537fa42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ac71d7b43e7a5cade16dfef10399c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147ef6b5b2882306dfc7",
+    "url": "/static/css/main.7679dc3c.chunk.css"
+  },
+  {
+    "revision": "f289e99ce49705dd5f6b",
+    "url": "/static/js/2.4296075f.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.4296075f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147ef6b5b2882306dfc7",
+    "url": "/static/js/main.ee9a8d81.chunk.js"
+  },
+  {
+    "revision": "f35af6f7ccead2fe5dbb",
+    "url": "/static/js/runtime-main.4422d9b2.js"
+  }
+]);
